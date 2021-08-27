5ce650e517aea5d6f299dd151be2eccd2e1d4022
Reneerenn <renee.renn@yahoo.com>
2021-08-26 19:45:51 -0500

    added code to area class

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,7 +1,16 @@
 package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
+        Scanner input;
+        double radius;
+        double area;
+        input = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        radius = input.nextDouble();
+        area = (radius * radius) * 3.14;
+        System.out.println("The area of a circle with a radius of " + radius + " = " + area);
 
     }
 }

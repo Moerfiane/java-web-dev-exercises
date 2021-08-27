c9051e9f544cf646b36c0c55b3ae6a344a2fd092
Adam <amcquirk@gmail.com>
2021-08-26 19:41:17 -0500

    changes

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,25 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+
+
+    public static void main(String[] args) {
+
+        double radius;
+        double pi = 3.14;
+
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a Radius: ");
+        radius = input.nextDouble();
+        input.close();
+
+
+
+    }
+
+
+}

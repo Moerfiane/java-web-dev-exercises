ba08a4cdda40a719c36537509a4b49be65e0caf7
Lucy134 <67610043+Lucy134@users.noreply.github.com>
2021-08-26 19:39:09 -0500

    added Circle class

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+    private double radius;
+
+    public double getArea(double radius){
+        return radius * radius * Math.PI;
+    }
+}

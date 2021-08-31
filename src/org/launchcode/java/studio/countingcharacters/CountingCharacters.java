cb32d2d6d05cf54f9111e897b11d43c9ffef4c17
Baubs <pathogenic@gmail.com>
2021-08-30 19:11:40 -0500

    added counting

--- src/org/launchcode/java/studio/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studio/countingcharacters/CountingCharacters.java
@@ -9,6 +9,13 @@ public class CountingCharacters {
         String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
                 "Once you’ve done that, it’s pretty straightforward from there.";
         char[] charArray = phrase.toLowerCase(Locale.ROOT).toCharArray();
+        for(char x : charArray){
+            if(count.containsKey(x)){
+                count.put(x, count.get(x) + 1);
+            } else{
+                count.put(x, 1);
+            }
+        }
 
     }
 }
